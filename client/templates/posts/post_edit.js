72b91f45f2ece17a66577780a12e3c132da3d927
voltron2112 <mason.sten@gmail.com>
2015-07-11 18:44:47 -0600

    Actually use the error reporting.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,7 +1,7 @@
 Template.postEdit.events({
   "submit form": function(e){
     e.preventDefault();
-    
+
     var currentPostId = this._id;
 
     var postProperties = {
@@ -12,7 +12,7 @@ Template.postEdit.events({
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
-        alert(error.reason);
+        throwError(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
